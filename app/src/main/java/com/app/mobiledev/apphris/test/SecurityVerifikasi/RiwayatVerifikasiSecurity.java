8431441cb80edc533db0407c977b263b0ae5f9a9
mazud21 <hmazud@gmail.com>
2022-02-18 15:48:03 +0700

    add test security package add test calendar multiple(not implement on form)

---
+++ app/src/main/java/com/app/mobiledev/apphris/test/SecurityVerifikasi/RiwayatVerifikasiSecurity.java
@@ -0,0 +1,16 @@
+package com.app.mobiledev.apphris.test.SecurityVerifikasi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.mobiledev.apphris.R;
+
+public class RiwayatVerifikasiSecurity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riwayat_verifikasi_security);
+
+    }
+}
---
+++ app/src/main/java/com/app/mobiledev/apphris/test/SecurityVerifikasi/SecurityVerifikasi.java
@@ -0,0 +1,29 @@
+package com.app.mobiledev.apphris.test.SecurityVerifikasi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+
+import com.app.mobiledev.apphris.R;
+import com.app.mobiledev.apphris.visitor.RiwayatVisit;
+
+public class SecurityVerifikasi extends AppCompatActivity {
+
+    LinearLayout llSecVer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security_verifikasi);
+
+        llSecVer = findViewById(R.id.llSecVer);
+
+        llSecVer.setOnClickListener(v -> {
+            Intent intent= new Intent(SecurityVerifikasi.this, RiwayatVisit.class);
+            startActivity(intent);
+        });
+
+    }
+}
